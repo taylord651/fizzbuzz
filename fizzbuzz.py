446904f2dfd84a8dc78e8826201c3e02fb97e5d8
Domonique Taylor <taylord651@gmail.com>
2019-11-05 19:33:30 -0600

    Automatically loops through 1 - 100 and prints appropriate message

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,18 +1,21 @@
 
-def fizzbuzz_program(number):
-    number_int = int(number)
-    if number_int % 5 == 0 and number_int % 3 == 0:
-        return "FizzBuzz"
-    elif number_int % 5 == 0:
-        return "Buzz"
-    elif number_int % 3 == 0:
-        return "Fizz"
-    else:
-        return "Number is not divisible by 3 or 5"
+def fizzbuzz_program():
+    # Loops through numbers 1 - 100
+    for i in range(1,101):
+        #Checks if i (number) is divisible by 3 and 5 first
+        if i % 5 == 0 and i % 3 == 0:
+            print ("FizzBuzz")
+        elif i % 5 == 0:
+            print ("Buzz")
+        elif i % 3 == 0:
+            print ("Fizz")
+        else:
+            print ("Number is not divisible by 3 or 5")
 
 def main():
-    number = input("Enter a number between 1 and 100: ")
-    print (fizzbuzz_program(number))
+    # Calls the fizzbuzz_program
+    fizzbuzz_program()
 
+# Calls the main function
 if __name__ == '__main__':
     main()
